27060066aae20860b6fb47d715d235a87e92b216
roman.shafeyev <rs@premiumgis.com>
2012-07-06 10:27:48 +0300

    change busdb.sh script

--- sql/busdb.sh
+++ sql/busdb.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
+#!/bin/bash
 
+
+DIRECTORY="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR=$DIRECTORY/scripts
+echo 'Current directory:'
+echo $DIRECTORY
 if [ ! -n "$2" ]; then
   echo 'error! Please, write config path' 
   exit
 fi
 
-  . $2
+  . $DIRECTORY/$2
 
 case "$1" in
 "create")
     echo $PORT
-    sudo -u postgres createdb -h $HOST -p $PORT -U $USER $DATABASE -T $TEMPLATE_DB
+    createdb -h $HOST -p $PORT -U $USER $DATABASE -T $TEMPLATE_DB
     exit
 ;;
 "drop")
-    sudo -u postgres dropdb -h $HOST -p $PORT -U $USER $DATABASE
+    dropdb -h $HOST -p $PORT -U $USER $DATABASE
     exit
 ;;
 "make")
 
-     sudo -u postgres psql -f $SCRIPT_DIR/func_clear.sql -d  $DATABASE
-     sudo -u postgres psql -f $SCRIPT_DIR/db_clear.sql -d  $DATABASE
+     psql -f $SCRIPT_DIR/func_clear.sql -d  $DATABASE
+     psql -f $SCRIPT_DIR/db_clear.sql -d  $DATABASE
 
-     sudo -u postgres psql -f $SCRIPT_DIR/db_create.sql -d  $DATABASE
-     sudo -u postgres psql -f $SCRIPT_DIR/func_create.sql -d  $DATABASE
+     psql -f $SCRIPT_DIR/db_create.sql -d  $DATABASE
+     psql -f $SCRIPT_DIR/func_create.sql -d  $DATABASE
+     psql -f $SCRIPT_DIR/db_init.sql -d $DATABASE
     exit
 ;;
 esac
